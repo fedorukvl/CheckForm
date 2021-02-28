ae01ed694a7ae16c150edf6c1bada943c9c80293
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-28 15:08:03 +0300

    Создан компонент-заглушка Loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="vld-parent">
+        <loading :active.sync="isLoading" 
+        :can-cancel="true" 
+        :on-cancel="onCancel"
+        :is-full-page="fullPage"></loading>
+    </div>
+</template>
+
+<script>
+    // Import component
+    import Loading from 'vue-loading-overlay';
+    // Import stylesheet
+    import 'vue-loading-overlay/dist/vue-loading.css';
+    
+    export default {
+        data() {
+            return {
+                isLoading: true,
+                fullPage: true
+            }
+        },
+        components: {
+            Loading
+        },
+    }
+</script>
